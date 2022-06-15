c3ba68c01f52d9506a706f34e2c9c9fc0c63d329
MarcosC_18 <MarcosC_18@LAPTOP-PF4EE2SM>
2022-06-15 00:57:48 -0300

    registro de ciudadanos

--- TrabajoFinal_Grupo18/src/main/java/ar/edu/unju/fi/html/controller/CiudadanoController.java
+++ TrabajoFinal_Grupo18/src/main/java/ar/edu/unju/fi/html/controller/CiudadanoController.java
@@ -1,5 +1,14 @@
 package ar.edu.unju.fi.html.controller;
 
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/ciudadano")
 public class CiudadanoController {
 
+	@GetMapping("/nuevo")
+	public String getNuevoCiudadano(Model model) {	
+		return("registro_ciudadano");
+	}	
 }
