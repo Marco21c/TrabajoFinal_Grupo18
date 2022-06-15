42945ce4aed03991981d004f6e87d1ecd12acf02
MarcosC_18 <MarcosC_18@LAPTOP-PF4EE2SM>
2022-06-15 01:45:52 -0300

    estilo de registro de ciudadano

--- TrabajoFinal_Grupo18/src/main/java/ar/edu/unju/fi/html/controller/CiudadanoController.java
+++ TrabajoFinal_Grupo18/src/main/java/ar/edu/unju/fi/html/controller/CiudadanoController.java
@@ -1,9 +1,10 @@
 package ar.edu.unju.fi.html.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+@Controller
 @RequestMapping("/ciudadano")
 public class CiudadanoController {
 
